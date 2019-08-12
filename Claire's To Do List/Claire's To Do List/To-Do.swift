9ad4b4f9b4c9cf1f0c801079e239c2a9ef55e411
Apple <apple@Apples-MacBook-Pro-62.local>
2019-08-12 11:05:43 -0400

    iteration 0

---
+++ Claire's To Do List/Claire's To Do List/To-Do.swift
@@ -0,0 +1,20 @@
+//
+//  To-Do.swift
+//  Claire's To Do List
+//
+//  Created by Apple on 8/12/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = " "
+    var important = false
+    
+//    init(ToDoname : String, ToDoimportant : Bool) {
+//        name = ToDoname
+//        important = ToDoimportant
+//    }
+}
+
